b0410936ec854e1972f6dd6cec27832faa623f90
TreeIsLife2 <71183248+TreeIsLife2@users.noreply.github.com>
2024-08-18 19:02:33 +0200

    cleanup
    remove old versions, add one standard

---
+++ assets/discontinuedTimeline.js
@@ -0,0 +1,1 @@
+import{o as r,c as s,e as a}from"./init.js";const p=(e,t)=>{const n=e.__vccOpts||e;for(const[c,o]of t)n[c]=o;return n},_={};function i(e,t){return r(),s("span",null,"The discontinued timeline is work in progress")}const l=p(_,[["render",i]]),u=document.querySelector("#app");a(l).mount(u);
